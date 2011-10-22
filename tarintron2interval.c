b58fd1cc3dec3c21a9714fe9a9eabacda31a9630
Andrea Sboner <ans2077@med.cornell.edu>
2011-10-22 03:02:05 +0000

    Update: including gene annotations as filter
    git-svn-id: svn+ssh://svn.gersteinlab.org/repos/bioinfo/novelTARs@629 2fd4de08-f6cc-4d2b-b124-baeea9ba92f3

--- tarintron2interval.c
+++ tarintron2interval.c
@@ -6,13 +6,17 @@
 int main( int argc, char** argv) 
 {
   Array intervals;
+  Array genes;
+  Array overlaps;
+
   Interval* leftInterval;
   Interval* rightInterval;
   Interval* intron;
   int i,j;
 
-   // reading interval file
+   // reading tar interval file
   intervals = intervalFind_parseFile( argv[1], 0 );
+  genes = intervalFind_parseFile( argv[2], 0 );
 
   for( i=0; i<arrayMax( intervals)-1; i++) {
     leftInterval = arrp( intervals, i, Interval );
@@ -26,14 +30,25 @@ int main( int argc, char** argv)
     intron->start = leftInterval->end+1;
     intron->end = rightInterval->start-1;
     intron->name = hlr_strdup( leftInterval->name );
-    intron->subIntervals = arrayCreate( 1, SubInterval);
     intron->subIntervalCount = 1;
-    for( j=0; j<1; j++) {
-      SubInterval* subInterval = arrayp( intron->subIntervals, arrayMax( intron->subIntervals ), SubInterval );
-      subInterval->start = leftInterval->end+1;
-      subInterval->end = rightInterval->start-1;
+    intron->subIntervals = arrayCreate(intron->subIntervalCount , SubInterval);
+    SubInterval* subInterval = arrayp( intron->subIntervals, arrayMax( intron->subIntervals ), SubInterval );
+    subInterval->start = leftInterval->end+1;
+    subInterval->end = rightInterval->start-1;
+    
+    overlaps = intervalFind_getOverlappingIntervals( intron->chromosome, intron->start, intron->end ); 
+    if( arrayMax( overlaps ) == 0 ) {
+      printf("%s\n",intervalFind_writeInterval( intron ) );
+    } else {
+      unsigned short int contained=0;
+      for( j = 0; j< arrayMax( overlaps ); j++) {
+	Interval* currOverlap = arrp( overlaps, j, Interval );
+	if( (intron->start <= currOverlap->start) && ( intron->end >= currOverlap->end ) ) contained=1; // checking if fully contained
+      }
+      if( !contained ) {
+	printf("%s\n",intervalFind_writeInterval( intron ) );
+      }
     }
-    printf("%s\n",intervalFind_writeInterval( intron ) );
     freeMem( intron );
   }
   return 0;
